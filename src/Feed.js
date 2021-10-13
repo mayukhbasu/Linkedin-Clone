94faec97b94a219e8ad96b7d276b68ead297715c
Bubul <bubul@Bubuls-MBP.Dlink>
2021-10-13 19:23:29 +0530

    building the feed section

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,11 @@
 import CreateIcon from "@material-ui/icons/Create";
 import React from 'react';
 import './Feed.css';
+import InputOption from "./InputOptions";
+import ImageIcon from "@material-ui/icons/Image";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
 
 const Feed = () => {
     return (
@@ -12,6 +17,16 @@ const Feed = () => {
                         <input type="text"/>
                         <button type="submit">Send</button>
                     </form>
+                </div>
+                <div className="feed__inputOptions">
+                <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+                <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+                <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+                <InputOption
+                Icon={CalendarViewDayIcon}
+                title="Write article"
+                color="#7FC15E"
+            />
                 </div>
             </div>
         </div>
